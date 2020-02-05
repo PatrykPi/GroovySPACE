0364bfdebd1ea17da30f89daddd2a5f59078d2ed
Patryk <piskorz.patryk.96@gmail.com>
2020-02-05 18:05:51 +0100

    Prallax background and paragraph

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,7 @@ module.exports = {
                         plugins: ['@babel/plugin-proposal-object-rest-spread']
                     }
                 }
-            },
-            
+            },    
             {
                 test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
                 use: [
