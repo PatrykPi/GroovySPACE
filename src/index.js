b550525a85d91eb5224b9d9df610949db35e5d08
Emilia Bywalec <emibyw2000@gmail.com>
2020-02-20 17:48:04 +0100

    About (instruments) styles 2

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import "./main.scss";
 
 import "./js/welcome.js";
 import "./js/MainNav-toggler.js";
+import "./js/instruments.js";
+
+import "./assets/images/drum-bg.jpg";
 import "./assets/images/piano.jpg";
+
+import "./assets/images/instruments-mic.jpg";
+import "./assets/images/instruments-guitar.jpg";
+import "./assets/images/instruments-drums.jpg";
+import "./assets/images/instruments-keyboard.jpg";
+import "./assets/images/instruments-speakers.jpg";
